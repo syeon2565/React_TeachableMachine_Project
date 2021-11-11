4078e7e19de91c6453f6de3b0ef813bad79146ca
seongyeon <ysy2565@naver.com>
2021-11-12 00:01:27 +0900

    route

--- camble/src/App.js
+++ camble/src/App.js
@@ -1,15 +1,31 @@
 import React from "react";
-// import Login from "./components/Login";
-// import Api from "./components/Api";
+import Login from "./components/Login";
+import { Route, Routes } from "react-router-dom";
+import Api from "./components/Api";
 import Main from "./components/Main";
+import styled from "styled-components";
 
-const App=()=>{
+const Logo = styled.div`
+width: 463px;
+height: 164px;
+font-family: Ramaraja;
+font-size: 72px;
+line-height: 123px;
+color: #000000;
+padding-left: 67px;
+padding-top: 15px;
+`;
+
+const App = () => {
   return (
-<>
-    {/* <Login/> */}
-    {/* <Api/> */}
-    <Main/>
-    </>
+    <div>
+      <Logo>Camble</Logo>
+      <Routes>
+        <Route path="/" element={<Main/>} />
+        <Route path="/api" element={<Api/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </div>
   );
 }
 export default App;
