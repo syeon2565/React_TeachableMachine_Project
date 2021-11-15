25ac74bf1a2bd5ed2cf067fca6f11043bcca0867
seongyeon <ysy2565@naver.com>
2021-11-15 20:21:19 +0900

    navbar

--- camble/src/components/Navbar.js
+++ camble/src/components/Navbar.js
@@ -2,6 +2,37 @@ import React, { Component } from 'react';
 import KakaoLogin from 'react-kakao-login';
 import styled from 'styled-components';
 
+
+const Container = styled.div`
+display: flex;
+flex-flow: row;
+`
+const Logo = styled.div`
+  width: 463px;
+  height: 164px;
+  font-family: Ramaraja;
+  font-size: 72px;
+  line-height: 123px;
+  color: #000000;
+  padding-left: 67px;
+  padding-top: 15px;
+  
+`;
+const KakaoButton = styled(KakaoLogin)`
+    padding: 0px;
+    line-height: 44px;
+    color: #000000;
+    background-color: #FFEB00;
+    border: 1px solid transparent;
+    border-radius: 3px;
+    font-size: 20px;
+    font-weight: bold;
+    text-align: center;
+    height: 82px;
+    width: 291px;
+    left: 300px;
+    top: -307px;
+`
 class Login extends Component {
 
     constructor(props) {
@@ -29,8 +60,9 @@ class Login extends Component {
 
     render() {
         return (
-            <Container>
-               
+            <div>
+             <Container>
+               <Logo>Camble</Logo>
                 <KakaoButton
                     jsKey={process.env.REACT_APP_Kakao}
                     buttonText="Kakao"
@@ -38,30 +70,12 @@ class Login extends Component {
                     onFailure={this.responseFail}
                     getProfile="true"
                 />
-            </Container>
+             </Container>
+                </div>
         );
     }
 }
 
-const Container = styled.div`
-    display: flex;
-    flex-flow: column wrap;
-`
 
-const KakaoButton = styled(KakaoLogin)`
-    padding: 0;
-    width: 190px;
-    height: 44px;
-    line-height: 44px;
-    color: #783c00;
-    background-color: #FFEB00;
-    border: 1px solid transparent;
-    border-radius: 3px;
-    font-size: 16px;
-    font-weight: bold;
-    text-align: center;
-    top:50px;
-    right:60px;
-`
 
 export default Login;
