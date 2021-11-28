d4a013d1d616b8b569795e89f83d4a9b58ff5cc6
seongyeon <ysy2565@naver.com>
2021-11-28 14:47:31 +0900

    tutorial link component

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -33,45 +33,55 @@ const Nav = styled.div`
   color: #ffffff;
 `;
 
-// const But = styled.div`
-//   height: 34px;
-//   width: 96px;
-//   left: 450px;
-//   top: 500px;
-//   border-radius: 0px;
-//   text-align: center;
-//   position: absolute;
-//   background: #c4c4c4;
-// `;
-// const Text = styled.div`
-//   height: 44px;
-//   width: 200px;
-//   left: 66px;
-//   top: -78px;
-//   border-radius: nullpx;
-//   padding-left: 400px;
-//   font-size: 20px;
-//   color: #ffffff;
-// `;
+const Button = styled.div`
+  height: 34px;
+  width: 96px;
+  left: 450px;
+  top: 500px;
+  line-height: 30px;
+  border-radius: 10px;
+  text-align: center;
+  position: absolute;
+  background: #777;
+  cursor: pointer;
+`;
+const Text = styled.div`
+  height: 44px;
+  width: 220px;
+  left: 66px;
+  border-radius: nullpx;
+  padding-left: 400px;
+  font-size: 20px;
+  color: #ffffff;
+`;
 
-var i=0;
+var i = 0;
+
 const Tutorial = () => {
-    
-    const [text, setText] = useState(
-        "웹 캠 사용을 허락한 후, 잠시 기다려주세요!"
-    );
-    const array =  ["자신의 얼굴과 어깨선을 기준으로, 움직이는 자세에 따라 거북목인지 정상인지 나타납니다.",
+    const [tuto, setTuto] = useState(true);
+  const [text, setText] = useState(
+    "웹 캠 사용을 허락한 후, 잠시 기다려주세요!"
+  );
+  const array = [
+    "자신의 얼굴과 어깨선을 기준으로, 움직이는 자세에 따라 거북목인지 정상인지 나타납니다.",
     "범위에 따라 거북목일 때의 비율이 높을 시 알림으로 경각심을 줄 수 있습니다.",
-    "자신의 거북목 측정값을 마이페이지에 기록하세요!"]
-    const onClickNext = () =>{
-    if(i!==3){
-        setText(array[i++]);
+    "자신의 거북목 측정값을 마이페이지에 기록하세요!",
+  ];
+  
+  const onClickNext = () => {
+    if (i !== array.length) {
+      setText(array[i++]);
+      console.log(tuto);
+      console.log(i);
+      document.getElementById("two_button").style.display="none"
+      if (i === 3) {
+        setTuto(false); //else로 가지고왔더니.. false값이 처음에 바뀌지 않아 우선 이렇게 해결...
+        document.getElementById("one_button").style.display="none";
+        document.getElementById("two_button").style.display="block";
+      }
     }
-    // else {
-    //     <Link to="../api">
-    //       </Link>
-    // }
-}
+    console.log(tuto);
+  };
 
   return (
     <div>
@@ -81,8 +91,11 @@ const Tutorial = () => {
           <Link to="../api" style={{ textDecoration: "none", color: "blue" }}>
             건너뛰기
           </Link>
-          <button onClick={onClickNext}>Next</button>
-          <h1>{text}</h1>
+          <Button id="one_button" onClick={onClickNext}>Next</Button>
+          <Link id="two_button" to="../api" style={{ textDecoration: "none", color: "blue", display: "none"}}>
+            <Button>Next</Button>
+          </Link>
+          <Text>{text}</Text>
         </ModalContainer>
       </Background>
     </div>
